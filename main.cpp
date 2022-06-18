5b5b3d6d816c97c638052e22f2df1bc5b791862c
emanueljuliano <emanueljulianoms@gmail.com>
2022-06-18 18:36:00 -0300

    pequenas identações

--- main.cpp
+++ main.cpp
@@ -57,69 +57,69 @@ void halt(const Instruction &instruction);
 int main()
 {
 
-    read_instructions();
+	read_instructions();
 
-    pass_one();
+	pass_one();
 
-    pass_two();
+	pass_two();
 
-    return 0;
+	return 0;
 }
 
 
 void read_instructions()
 {
-    while (true)
-    {
-        string input;
-        getline(cin, input);
-        if (input == "END")
-            break;
+	while (true)
+	{
+		string input;
+		getline(cin, input);
+		if (input == "END")
+			break;
 
-        instructions.push_back(Instruction(input));
-    }
+		instructions.push_back(Instruction(input));
+	}
 }
 
 void pass_one()
 {
-    ILC = 0;
-    for (auto instruction : instructions)
-    { 
-        //cout << instruction.operator_str << ' ' << instruction.operator_code << ' ' << instruction.operand << endl;
+	ILC = 0;
+	for (auto instruction : instructions)
+	{ 
+		//cout << instruction.operator_str << ' ' << instruction.operator_code << ' ' << instruction.operand << endl;
 
-        if (instruction.label != "") 
-            symbol_table.insert(Symbol(instruction.label, ILC));
-        if (instruction.is_pseudo) // pseudo instrução WORD
-        {
-            // determinar espaço para dados
-        }
-        else
-        {
-            // pesquisar tabela de operandos 
+		if (instruction.label != "") 
+			symbol_table.insert(Symbol(instruction.label, ILC));
+		if (instruction.is_pseudo) // pseudo instrução WORD
+		{
+			// determinar espaço para dados
+		}
+		else
+		{
+			// pesquisar tabela de operandos 
 			// (ja sabemos o operando, entao acho que nao precisa)
-            // processar literais (acho que não precisa)
-        }
+			// processar literais (acho que não precisa)
+		}
 
-        ILC += instruction.size;
-    }
+		ILC += instruction.size;
+	}
 }
 
 void pass_two()
 {
 	int size = ILC;
-    ILC = 0;
+	ILC = 0;
 	cout << "MVI ";
-    cout << ILC << ' ';
+	cout << ILC << ' ';
 	cout << MEMORY_SIZE -1 << ' ';
 	cout << size << ' ';
 
 	for (auto instruction : instructions)
-    {
-        if (!instruction.is_pseudo)
-            process_instruction(instruction);
-		
+	{
+		if (!instruction.is_pseudo)
+			process_instruction(instruction);
+
 		// output
-        if (!instruction.is_pseudo) {
+		if (!instruction.is_pseudo) {
 			cout << instruction.operator_code << ' ';
 
 			if (instruction.size > 1) {
@@ -129,66 +129,66 @@ void pass_two()
 		}
 		else
 			cout << 0 << ' ';
-        ILC += instruction.size;
-    }
+		ILC += instruction.size;
+	}
 	cout << endl;
 }
 
 void process_instruction(const Instruction &instruction)
 {
-    switch (instruction.operator_code)
-    {
-    case LOAD:
-        load(instruction);
-        break;
-    case STORE:
-        store(instruction);
-        break;
-    case ADD:
-        add(instruction);
-        break;
-    case SUB:
-        sub(instruction);
-        break;
-    case JMP:
-        jmp(instruction);
-        break;
-    case JPG:
-        jpg(instruction);
-        break;
-    case JPL:
-        jpl(instruction);
-        break;
-    case JPE:
-        jpe(instruction);
-        break;
-    case JPNE:
-        jpne(instruction);
-        break;
-    case PUSH:
-        push(instruction);
-        break;
-    case POP:
-        pop(instruction);
-        break;
-    case READ:
-        read(instruction);
-        break;
-    case WRITE:
-        write(instruction);
-        break;
-    case CALL:
-        call(instruction);
-        break;
-    case RET:
-        ret(instruction);
-        break;
-    case HALT:
-        halt(instruction);
-        break;
-    default:
-        break;
-    }
+	switch (instruction.operator_code)
+	{
+		case LOAD:
+			load(instruction);
+			break;
+		case STORE:
+			store(instruction);
+			break;
+		case ADD:
+			add(instruction);
+			break;
+		case SUB:
+			sub(instruction);
+			break;
+		case JMP:
+			jmp(instruction);
+			break;
+		case JPG:
+			jpg(instruction);
+			break;
+		case JPL:
+			jpl(instruction);
+			break;
+		case JPE:
+			jpe(instruction);
+			break;
+		case JPNE:
+			jpne(instruction);
+			break;
+		case PUSH:
+			push(instruction);
+			break;
+		case POP:
+			pop(instruction);
+			break;
+		case READ:
+			read(instruction);
+			break;
+		case WRITE:
+			write(instruction);
+			break;
+		case CALL:
+			call(instruction);
+			break;
+		case RET:
+			ret(instruction);
+			break;
+		case HALT:
+			halt(instruction);
+			break;
+		default:
+			break;
+	}
 }
 
 void load(const Instruction &instruction) {}
--- symbol_table.cpp
+++ symbol_table.cpp
@@ -7,15 +7,15 @@ SymbolTable::SymbolTable()
 
 void SymbolTable::insert(Symbol new_symbol)
 {
-    table.push_back(new_symbol);
+	table.push_back(new_symbol);
 }
 
 int SymbolTable::find(string operand)
 {
-    int ret = -1;
-    for (auto symbol : table) {
-	if (symbol.symbol == operand)
-	ret = symbol.value;
-    }
-    return ret;
+	int ret = -1;
+	for (auto symbol : table) {
+		if (symbol.symbol == operand)
+			ret = symbol.value;
+	}
+	return ret;
 }
